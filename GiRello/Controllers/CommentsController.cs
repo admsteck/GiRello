0deb7eccccf1bfe48e774b1da0ceefa2d787f1ef
Adam Stechschulte <admsteck@thestechschultes.com>
2012-08-23 12:52:11 -0400

    fix extra using

--- GiRello/Controllers/CommentsController.cs
+++ GiRello/Controllers/CommentsController.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using TrelloNet;
-using GiRello.Models;
 using Newtonsoft.Json;
 using System.Web;
 using System.Text.RegularExpressions;
