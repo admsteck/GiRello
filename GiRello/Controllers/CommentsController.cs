be44bcdfbfe6017924cd78d95593bc7d95c94cbb
Adam Stechschulte <admsteck@thestechschultes.com>
2012-08-23 08:11:28 -0400

    Project start

---
+++ GiRello/Controllers/CommentsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GiRello.Controllers
+{
+    public class CommentsController : ApiController
+    {
+        // GET api/comments
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/comments/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/comments
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/comments/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/comments/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 GiRello
 =======
+
+GiRello is a .Net project to allow GitHub or Bitbucket commits to interact with Trello
