74f6e42f5533d2d13c96fb381f8586be3d0af221
Adam Stechschulte <admsteck@thestechschultes.com>
2012-08-24 10:53:07 -0400

    #5 Handle payload better

--- GiRello/Controllers/CommentsController.cs
+++ GiRello/Controllers/CommentsController.cs
@@ -1,37 +1,45 @@
 ﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Http;
 using TrelloNet;
-using System.Linq;
 
 namespace GiRello.Controllers
 {
     public class CommentsController : ApiController
     {
         // POST api/comments
-        public void Post([FromUri] string id)
+        public HttpResponseMessage Post([FromUri] string id, [FromBody] Models.GitPost gitPost)
         {
-            var content = Request.Content.ReadAsStringAsync();
-            content.Wait();
-            var result = content.Result;
-            result = HttpUtility.UrlDecode(result);
-            result = result.Replace("payload=", "");
-            dynamic payload = JsonConvert.DeserializeObject(result);
-            foreach (dynamic commit in payload.commits)
+            HttpResponseMessage response;
+            if (ModelState.IsValid && gitPost != null)
             {
-                string message = commit.message;
-                string pattern = @"(?<=#)\d{1,}";
-                foreach (Match match in Regex.Matches(message, pattern, RegexOptions.IgnoreCase))
+                response = Request.CreateResponse(HttpStatusCode.NoContent);
+                dynamic payload = JsonConvert.DeserializeObject(gitPost.payload);
+                foreach (dynamic commit in payload.commits)
                 {
-                    var trello = new Trello("c6163a4015c586e703e8ea98f94a89fa");
-                    trello.Authorize("8c9152a54ff0b43f28d97966621ccb88242b703bfb4de005e3d2ab319da3ad54");
-                    var boardId = new BoardId(id);
-                    var card = trello.Cards.WithShortId(int.Parse(match.Value), boardId);
-                    var cardId = new CardId(card.Id);
-                    trello.Cards.AddComment(cardId, "Commit added: " + message);
+                    string message = commit.message;
+                    string pattern = @"(?<=#)\d{1,}";
+                    foreach (Match match in Regex.Matches(message, pattern, RegexOptions.IgnoreCase))
+                    {
+                        var trello = new Trello("c6163a4015c586e703e8ea98f94a89fa");
+                        trello.Authorize("8c9152a54ff0b43f28d97966621ccb88242b703bfb4de005e3d2ab319da3ad54");
+                        var boardId = new BoardId(id);
+                        var card = trello.Cards.WithShortId(int.Parse(match.Value), boardId);
+                        var cardId = new CardId(card.Id);
+                        trello.Cards.AddComment(cardId, "Commit added: " + message);
+                        response = Request.CreateResponse(HttpStatusCode.Created);
+                    }
                 }
+
             }
+            else
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            return response;
         }
 
     }
---
+++ GiRello/Models/GitPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GiRello.Models
+{
+    public class GitPost
+    {
+        [Required]
+        public string payload { get; set; }
+    }
+}
