a926194b041f3f07f0f1eda63e3c074dfc4903a3
Adam Stechschulte <admsteck@thestechschultes.com>
2012-08-27 10:20:38 -0400

    Revert "Move db initializer to Application_Start"
    This reverts commit 9f302aa203a44651cf329eecce6bf12f2e8b9f5a.

--- GiRello/Models/Auth.cs
+++ GiRello/Models/Auth.cs
@@ -10,6 +10,11 @@ namespace GiRello.Models
     public class AuthContext : DbContext
     {
         public DbSet<Auth> Auths { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AuthContext, GiRello.Migrations.Configuration>());
+        }
     }
 
     public class Auth
