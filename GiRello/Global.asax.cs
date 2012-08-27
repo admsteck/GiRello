9f302aa203a44651cf329eecce6bf12f2e8b9f5a
Adam Stechschulte <admsteck@thestechschultes.com>
2012-08-27 09:24:17 -0400

    Move db initializer to Application_Start

--- GiRello/Global.asax.cs
+++ GiRello/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -22,6 +23,8 @@ namespace GiRello
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Models.AuthContext, Migrations.Configuration>());
         }
     }
 }
