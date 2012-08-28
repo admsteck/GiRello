a3434b0c4a6ffbdd3d8fd36deb5df1089f0cd383
Adam Stechschulte <admsteck@thestechschultes.com>
2012-08-27 20:35:50 -0400

    Refactor context out of auth class and rename

--- GiRello/Models/Auth.cs
+++ GiRello/Models/Auth.cs
@@ -7,16 +7,6 @@ using System.Web;
 
 namespace GiRello.Models
 {
-    public class AuthContext : DbContext
-    {
-        public DbSet<Auth> Auths { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AuthContext, GiRello.Migrations.Configuration>());
-        }
-    }
-
     public class Auth
     {
         [Key, Required]
---
+++ GiRello/Models/GiRelloContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GiRello.Models
+{
+    public class GiRelloContext : DbContext
+    {
+        public DbSet<Auth> Auths { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<GiRelloContext, GiRello.Migrations.Configuration>());
+        }
+    }
+}
