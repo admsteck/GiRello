9a5c3463d9bf53327852c86583ca64d05f8af30e
Adam Stechschulte <admsteck@thestechschultes.com>
2012-08-28 00:42:38 -0400

    Added jquery.blockUI.js for #16

--- GiRello/App_Start/BundleConfig.cs
+++ GiRello/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace GiRello
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery.blockUI").Include(
+                        "~/Scripts/jquery.blockUI.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
